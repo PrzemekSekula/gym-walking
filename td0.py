0fccde0787b77a3f54a77ca98737f4e582343854
PrzemekS <przemeksekula@gmail.com>
2023-04-04 09:31:30 +0200

    chore: minor updates in mc and td files

--- td0.py
+++ td0.py
@@ -21,8 +21,8 @@ parser.add_argument('-g', '--gamma',
 
 parser.add_argument('-a', '--alpha',
                     type=float,
-                    default=0.5,
-                    help='Alpha parameter for td target formula. Default: 0.5'
+                    default=0.05,
+                    help='Alpha parameter for td target formula. Default: 0.05'
                    )
 
 parser.add_argument('-r', '--render',
@@ -35,19 +35,21 @@ parser.add_argument('-r', '--render',
 env = gym.make('Walking5-v0')
 pi = lambda x: np.random.randint(2) # uniform random policy
 
-def td(pi, env, gamma=1.0, alpha=0.5, n_episodes=10, render=True):
+def td(pi, env, gamma=1.0, alpha=0.05, n_episodes=10, render=True):
     V = np.array([0] + [0.5] * (env.observation_space.n - 2) + [0])
     for t in range(n_episodes):
         state, info = env.reset()
         if render:
             env.render(V)
-        done = False
-        while not done:
+        terminal = False
+        while not terminal:
             action = pi(state)
-            next_state, reward, done, truncated, info = env.step(action)
-            td_target = reward + gamma * V[next_state]
+            next_state, reward, terminal, truncated, info = env.step(action)
+            
+            # V(terminal state) is zero by definition
+            td_target = reward + gamma * (V[next_state] if not terminal else 0)
             td_error = td_target - V[state]
-            V[state] = V[state] + alpha * td_error
+            V[state] += alpha * td_error
             state = next_state
             if render:
                 env.render(V)
